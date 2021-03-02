99dafe33b1fcff5ab1914088482ed52421ceb5a3
everyoned <33046044+everyoned@users.noreply.github.com>
2021-03-02 15:45:19 +0800

    Update test.py

--- test.py
+++ test.py
@@ -4,6 +4,8 @@ import argparse
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='manual to this script')
     parser.add_argument('--token', type=str, default=None)
+    parser.add_argument('--locLat', type=str, default=None)
+    parser.add_argument('--locLng', type=str, default=None)
     args = parser.parse_args()
     print("用户信息：", args)
     now = (datetime.datetime.now() + datetime.timedelta(hours=8)).strftime("%Y-%m-%d %H:%M:%S")
