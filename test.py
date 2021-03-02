8096042dd2431397035e4df72c55bc7964d92463
everyoned <33046044+everyoned@users.noreply.github.com>
2021-03-02 15:21:10 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='manual to this script')
+    parser.add_argument('--token', type=str, default=None)
+    args = parser.parse_args()
+    print("用户信息：", args)
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(now)
