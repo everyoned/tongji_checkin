dcda5354a5f2a1bab7b4a63514c22d156a88ecc1
everyoned <33046044+everyoned@users.noreply.github.com>
2021-03-02 15:36:34 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+import datetime
 import argparse
 
 if __name__ == "__main__":
@@ -6,5 +6,5 @@ if __name__ == "__main__":
     parser.add_argument('--token', type=str, default=None)
     args = parser.parse_args()
     print("用户信息：", args)
-    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    now = (datetime.datetime.now() + datetime.timedelta(hours=8)).strftime("%Y-%m-%d %H:%M:%S")
     print(now)
